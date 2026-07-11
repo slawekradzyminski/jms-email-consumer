1baacc086997434ca0eba3a363906d96099f9c62
Slawek Radzyminski <14099756+slawekradzyminski@users.noreply.github.com>
2026-07-12 00:56:33 +0200

    Upgrade Spring Boot to 4.1.0

--- src/main/java/com/awesome/testing/listener/JmsConfig.java
+++ src/main/java/com/awesome/testing/listener/JmsConfig.java
@@ -5,12 +5,12 @@ import java.util.Map;
 import jakarta.jms.ConnectionFactory;
 
 import com.awesome.testing.dto.email.EmailDto;
-import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+import org.springframework.boot.jms.autoconfigure.DefaultJmsListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 import org.springframework.jms.config.JmsListenerContainerFactory;
-import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.JacksonJsonMessageConverter;
 import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
 
@@ -29,7 +29,7 @@ public class JmsConfig {
 
     @Bean
     public MessageConverter jacksonJmsMessageConverter() {
-        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        JacksonJsonMessageConverter converter = new JacksonJsonMessageConverter();
         converter.setTargetType(MessageType.TEXT);
         converter.setTypeIdPropertyName("_awesome_");
         converter.setTypeIdMappings(Map.of(
